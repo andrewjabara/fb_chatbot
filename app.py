f9d93ad330c9ad571fc945942184ae5d97dad923
andrewjabara <andrewjabara@gmail.com>
2017-02-18 15:55:47 -0800

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def verify():
     # when the endpoint is registered as a webhook, it must echo back
     # the 'hub.challenge' value it receives in the query arguments
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == test_token
+        if not request.args.get("hub.verify_token") == "test_token"
         #os.environ["VERIFY_TOKEN"]
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
@@ -58,7 +58,7 @@ def send_message(recipient_id, message_text):
     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
 
     params = {
-        "access_token": EAAFcsXfJeGMBAKhrV8PRwP2nyvxMzTYSqdmmZBXGuqv2Lz3gJ2YHDFWHP2jbZBvpxrL5sV0cs1iOVhmEtRDE12o5LKSgNfAvUjdq7ZAZAsu9VbBi8grZC4TZC1WKM7OpCbi5etZBU0ZBIchej9CLCTeqXnbyWbP6YFAXOLxL3mOk6QZDZD
+        "access_token": "EAAFcsXfJeGMBAKhrV8PRwP2nyvxMzTYSqdmmZBXGuqv2Lz3gJ2YHDFWHP2jbZBvpxrL5sV0cs1iOVhmEtRDE12o5LKSgNfAvUjdq7ZAZAsu9VbBi8grZC4TZC1WKM7OpCbi5etZBU0ZBIchej9CLCTeqXnbyWbP6YFAXOLxL3mOk6QZDZD"
         os.environ["PAGE_ACCESS_TOKEN"]
         # EAAFcsXfJeGMBAKhrV8PRwP2nyvxMzTYSqdmmZBXGuqv2Lz3gJ2YHDFWHP2jbZBvpxrL5sV0cs1iOVhmEtRDE12o5LKSgNfAvUjdq7ZAZAsu9VbBi8grZC4TZC1WKM7OpCbi5etZBU0ZBIchej9CLCTeqXnbyWbP6YFAXOLxL3mOk6QZDZD
     }
