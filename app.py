e3deab9768521f8ed3d0a257b5f06d3baaf24f2b
andrewjabara <andrewjabara@gmail.com>
2017-02-18 16:06:34 -0800

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ def verify():
     # the 'hub.challenge' value it receives in the query arguments
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
         if not request.args.get("hub.verify_token") == "test_token"
+        
+        #test_token
         #os.environ["VERIFY_TOKEN"]
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
@@ -59,7 +61,8 @@ def send_message(recipient_id, message_text):
 
     params = {
         "access_token": "EAAFcsXfJeGMBAKhrV8PRwP2nyvxMzTYSqdmmZBXGuqv2Lz3gJ2YHDFWHP2jbZBvpxrL5sV0cs1iOVhmEtRDE12o5LKSgNfAvUjdq7ZAZAsu9VbBi8grZC4TZC1WKM7OpCbi5etZBU0ZBIchej9CLCTeqXnbyWbP6YFAXOLxL3mOk6QZDZD"
-        os.environ["PAGE_ACCESS_TOKEN"]
+        
+        #os.environ["PAGE_ACCESS_TOKEN"]
         # EAAFcsXfJeGMBAKhrV8PRwP2nyvxMzTYSqdmmZBXGuqv2Lz3gJ2YHDFWHP2jbZBvpxrL5sV0cs1iOVhmEtRDE12o5LKSgNfAvUjdq7ZAZAsu9VbBi8grZC4TZC1WKM7OpCbi5etZBU0ZBIchej9CLCTeqXnbyWbP6YFAXOLxL3mOk6QZDZD
     }
     headers = {
